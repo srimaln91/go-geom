170b879f5576ba2f50fcc800165c6761692f8672
srimal_n <srimal@pickme.lk>
2019-04-24 12:53:13 +0530

    Avoid using by accident non _r functions

--- geos/geos.h
+++ geos/geos.h
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+
+// Avoid using by accident non _r functions
+#ifndef GEOS_USE_ONLY_R_API
+#define GEOS_USE_ONLY_R_API
+#endif
+
 #include <geos_c.h>
 
 void notice(const char *fmt, ...);
