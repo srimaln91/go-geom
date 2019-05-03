31d3a3be1dac0dc4691d347ccd89d0914e734f2a
srimal_n <srimal@pickme.lk>
2019-05-03 11:07:23 +0530

    Implemented BufferWithOptions(), BufferWithStyles()

---
+++ geos/bufferparams_test.go
@@ -0,0 +1,51 @@
+package geos
+
+import (
+	"testing"
+)
+
+func TestBufferWithParams(t *testing.T) {
+	bParams, _ := CreateBufferParams()
+	defer bParams.Destroy()
+
+	quadsegs := 8
+	width := 10.0
+
+	bParams.SetEndCapStyle(GEOSBUF_CAP_ROUND)
+	bParams.SetJoinStyle(GEOSBUF_JOIN_ROUND)
+	bParams.SetQuadrantSegments(quadsegs)
+	bParams.SetMitreLimit(5)
+
+	geom := FromWKT("POINT(0 0)")
+
+	geom.BufferWithParams(width, bParams)
+
+	ptCount, _ := geom.GetNumCoordinates()
+
+	if geom == nil || ptCount != 33 {
+		t.Errorf("Error: CreateBufferWithParams()")
+	}
+
+	geom.Destroy()
+}
+
+func TestSetSingleSided(t *testing.T) {
+	bParams, _ := CreateBufferParams()
+	defer bParams.Destroy()
+
+	width := 10.0
+
+	bParams.SetSingleSided()
+
+	geom := FromWKT("LINESTRING(0 0, 1 1)")
+
+	geom.BufferWithParams(width, bParams)
+
+	ptCount, _ := geom.GetNumCoordinates()
+
+	if geom == nil || ptCount != 5 {
+		t.Errorf("Error: SetSingleSided()")
+	}
+
+	geom.Destroy()
+}
