906b3807de5ba1d11d800e2201a2a4f4e5d5aba7
srimal_n <srimal@pickme.lk>
2019-07-26 23:40:18 +0530

    Added EPSG:4978 coordinate system

--- geom/lwgeom.go
+++ geom/lwgeom.go
@@ -25,6 +25,7 @@ type Geom struct {
 // SRS Stores required spatial reference systems
 var SRS = map[string]string{
 	"EPSG:4326": "+proj=longlat +datum=WGS84 +no_defs",
+	"EPSG:4978": "+proj=geocent +datum=WGS84 +units=m +no_defs",
 	"EPSG:3857": "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs",
 }
 
